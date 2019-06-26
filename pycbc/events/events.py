eece55a8f60e3d4ae75e598b707569ccd45aa686
Sumit Kumar <sumit.kumar@atlas2>
2019-06-26 13:26:20 +0000

    Reduced threshold in NewSNRSGPSD from 1.8 to 1.15

--- pycbc/events/events.py
+++ pycbc/events/events.py
@@ -226,7 +226,7 @@ def newsnr_sgveto_psdvar(snr, bchisq, sgchisq, psd_var_val):
     nsnr = newsnr_sgveto(snr, bchisq, sgchisq)
     nsnr = numpy.array(nsnr, ndmin=1)
     psd_var_val = numpy.array(psd_var_val, ndmin=1)
-    t = numpy.array(psd_var_val >= 1.8, ndmin=1)
+    t = numpy.array(psd_var_val >= 1.15, ndmin=1)
     if len(t) > 0:
         nsnr[t] = nsnr[t] / numpy.sqrt(psd_var_val[t])
 
